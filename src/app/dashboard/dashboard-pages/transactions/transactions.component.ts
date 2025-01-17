f4f9c3f0ba06dd63511f594a8f716d345f80e214
kinggiddy1 <kinggiddy@gmail.com>
2025-01-17 19:37:06 +0200

    added a data table

--- src/app/dashboard/dashboard-pages/transactions/transactions.component.ts
+++ src/app/dashboard/dashboard-pages/transactions/transactions.component.ts
@@ -1,34 +1,47 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TransService } from '../../../shared/services/trans.service';
-import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-//DATA TABLE
+import { DataTablesModule } from 'angular-datatables';
+import { Subject } from 'rxjs';
 
 
 @Component({
   selector: 'app-transactions',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DataTablesModule],
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.css'
 })
 export class TransactionsComponent implements OnInit{
 
   transactions: any[] = []; 
+  dtOptions: DataTables.Settings = {}; 
+  dtTrigger: Subject<any> = new Subject<any>();
 
-  constructor(private transService: TransService, 
-    private router: Router){}
-  
+  constructor(private transService: TransService){}
+   
 
     ngOnInit(): void {
+
+      this.dtOptions = {
+        pagingType: 'full_numbers',
+        pageLength: 10,
+        responsive: true,
+      };
+
       this.transService.getTransactions().subscribe({
         next: (data) => {
           console.log(data);
           this.transactions = data.transactions;
+          this.dtTrigger.next(null);
         },
         error: (error) => {
           console.error('Error fetching Balance', error);
         }
       });
+    }
+
+    ngOnDestroy(): void {
+      this.dtTrigger.unsubscribe();
     }
 }
