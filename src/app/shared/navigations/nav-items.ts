7db02005a09655cbab3adb94c68f7d4bd3a9ad94
kinggiddy1 <kinggiddy@gmail.com>
2025-01-18 11:23:36 +0200

    added a new nav item

--- src/app/shared/navigations/nav-items.ts
+++ src/app/shared/navigations/nav-items.ts
@@ -2,7 +2,8 @@ export const NAV_ITEMS: Record<string, Array<{ label: string; link: string; icon
     Admin: [
       { label: 'Dashboard', link: '/dashboard', icon: 'house-check-fill' },
       { label: 'Transactions', link: '/transactions', icon: 'list-ol' },
-      { label: 'New Order', link: '/orders', icon: 'people' },
+      { label: 'New Order', link: '/orders', icon: 'sign-turn-right-fill' },
+      { label: 'New Income', link: '/incomes', icon: 'sign-turn-left-fill' },
     ],
  
   };
